860e4128b4953ac9059bf9c3fd3166790d147efd
erickmerchant <erickmerchant@gmail.com>
2025-03-09 11:08:18 -0400

    Import _nodes

--- mine-sweeper.js
+++ mine-sweeper.js
@@ -1,8 +1,9 @@
+import "handcraft/dom/_nodes.js";
+import "handcraft/dom/append.js";
 import "handcraft/dom/aria.js";
 import "handcraft/dom/attr.js";
 import "handcraft/dom/classes.js";
 import "handcraft/dom/effect.js";
-import "handcraft/dom/append.js";
 import "handcraft/dom/observe.js";
 import "handcraft/dom/on.js";
 import "handcraft/dom/shadow.js";
