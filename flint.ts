6e00a4a435a079f0d66843072dcf2a0f234f19b5
erickmerchant <erickmerchant@gmail.com>
2025-10-04 01:32:08 -0400

    Upgrade flint

--- flint.ts
+++ flint.ts
@@ -4,10 +4,10 @@ import flint from "@flint/framework";
 import index from "./src/index.ts";
 
 const app = flint("src", "dist")
-  .route("/", { handler: index })
-  .file("/index.css", { handler: css })
-  .file("/mine-sweeper.js", { handler: js })
-  .file("/mine-sweeper.css", { handler: css });
+  .route("/", index)
+  .file("/index.css", css)
+  .file("/mine-sweeper.js", js)
+  .file("/mine-sweeper.css", css);
 
 export default app;
 
