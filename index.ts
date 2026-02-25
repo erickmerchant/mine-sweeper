91116ba4c87ca6605989d7ed148e66ed4c62b808
erickmerchant <erickmerchant@gmail.com>
2026-02-25 14:33:04 -0500

    Updates

--- index.ts
+++ index.ts
@@ -19,14 +19,14 @@ export default function () {
         meta.charset("utf-8"),
         meta.name("viewport").content("width=device-width, initial-scale=1"),
         title("Mine-Sweeper"),
-        link.rel("stylesheet").href("/index.css"),
-        script.type("module").src("/mine-sweeper.js"),
+        link.rel("stylesheet").href("/index.css?inline"),
+        script.type("module").src("/mine-sweeper.js?inline"),
       ),
       body.class("page")(
         mineSweeper.count(10).height(8).width(8)
           .shadow(
             { mode: "open" },
-            link.rel("stylesheet").href("/mine-sweeper.css"),
+            link.rel("stylesheet").href("/mine-sweeper.css?inline"),
           ),
       ),
     ),
