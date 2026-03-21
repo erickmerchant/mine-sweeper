19386a7694af8ebddb687af74cf99757a8ae8143
erickmerchant <erickmerchant@gmail.com>
2026-03-21 01:34:54 -0400

    Upgrade

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
-import { h } from "@handcraft/lib";
-import { render } from "@handcraft/lib/render";
+import { h } from "@handcraft/lib/templating";
 
 const {
   html,
@@ -13,22 +12,20 @@ const {
 } = h.html;
 
 export default function () {
-  return render(() =>
-    html.lang("en-US")(
-      head(
-        meta.charset("utf-8"),
-        meta.name("viewport").content("width=device-width, initial-scale=1"),
-        title("Mine-Sweeper"),
-        link.rel("stylesheet").href("/index.css?inline"),
-        script.type("module").src("/mine-sweeper.js?inline"),
-      ),
-      body.class("page")(
-        mineSweeper.count(10).height(8).width(8)
-          .shadow(
-            { mode: "open" },
-            link.rel("stylesheet").href("/mine-sweeper.css?inline"),
-          ),
-      ),
-    )
+  return html.lang("en-US")(
+    head(
+      meta.charset("utf-8"),
+      meta.name("viewport").content("width=device-width, initial-scale=1"),
+      title("Mine-Sweeper"),
+      link.rel("stylesheet").href("/index.css?inline"),
+      script.type("module").src("/mine-sweeper.js?inline"),
+    ),
+    body.class("page")(
+      mineSweeper.count(10).height(8).width(8)
+        .shadow(
+          { mode: "open" },
+          link.rel("stylesheet").href("/mine-sweeper.css?inline"),
+        ),
+    ),
   );
 }
